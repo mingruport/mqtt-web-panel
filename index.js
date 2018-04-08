f9d94aaba7c1ac89e385e791cfa68e41724bfeaf
Ming Ruport <mingsruport@gmail.com>
2018-04-08 18:16:11 +0300

    Fix default error

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status).json({
-    message: err.message
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error'
   });
 });
 
