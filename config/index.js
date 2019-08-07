3826b5ce5547028e1858325c2e4c977b51b6ee5e
Alexander Gorkalov <mingsruport@gmail.com>
2019-08-07 11:08:32 +0300

    Update config and add .env.example

--- config/index.js
+++ config/index.js
@@ -1,17 +1,13 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
-const config = {};
-
-config.port = process.env.PORT || 3000;
-config.mongodbUIR = process.env.MONGOHQ_URL;
-
-config.mqttOptions = {
-  host: process.env.MQTT_URL,
-  port: process.env.MQTT_PORT,
-  username: process.env.MQTT_USERNAME,
-  password: process.env.MQTT_PASSWORD,
+module.exports = {
+  port: process.env.PORT || 3000,
+  mongodbUIR: process.env.MONGODB_URI,
+  timeZone: process.env.TIME_ZONE || '00:00',
+  mqttOptions: {
+    host: process.env.MQTT_URL,
+    port: process.env.MQTT_PORT,
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD,
+  }
 };
-
-config.timeZone = process.env.TIME_ZONE || '00:00';
-
-module.exports = config;
