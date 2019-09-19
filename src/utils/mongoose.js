c06331775e372b88bdeb2dc446ff7ce120a141c9
Alexander Gorkalov <mingsruport@gmail.com>
2019-09-19 16:27:59 +0300

    Update dependencies

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -4,8 +4,11 @@ const config = require('./../config');
 const isDev = config.env !== 'production';
 
 mongoose.Promise = global.Promise;
+
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 if (isDev) mongoose.set('debug', true);
 
-module.exports = mongoose.connect(config.mongodbUIR, { useNewUrlParser: true });
+module.exports = mongoose.connect(config.mongodbUIR);
