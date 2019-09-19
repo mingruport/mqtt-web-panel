528ee50d6ab9011e35d4c1d4deca2ac701cbf2e4
Alexander Gorkalov <mingsruport@gmail.com>
2019-09-19 16:13:56 +0300

    Remove body-parser

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const socketio = require('socket.io');
 const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const path = require('path');
 const favicon = require('serve-favicon');
 
@@ -23,7 +22,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(compression());
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(express.static('src/public'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
